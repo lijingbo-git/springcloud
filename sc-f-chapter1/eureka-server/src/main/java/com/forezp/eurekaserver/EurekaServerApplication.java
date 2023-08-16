bb5e255ed5718a73438aa7f7ffd71fb2460c4f89
lijingbo-git <38450499+lijingbo-git@users.noreply.github.com>
2023-08-16 22:43:24 +0800

    Add files via upload
    eureka,ribbon,feign

---
+++ sc-f-chapter1/eureka-server/src/main/java/com/forezp/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,24 @@
+package com.forezp.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 重要的事情说三遍：
+ * 文档地址： http://cloud.spring.io/spring-cloud-static/Finchley.RELEASE/single/spring-cloud.html
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( EurekaServerApplication.class, args );
+    }
+
+
+
+}
---
+++ sc-f-chapter1/service-ribbon/src/test/java/com/forezp/serviceribbon/ServiceRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forezp.serviceribbon;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
